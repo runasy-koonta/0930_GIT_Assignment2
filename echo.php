8e4e290fc773238fe8fc04cf6a5b12876fe5144c
Minjun Kang <minjun@kaaang.dev>
2022-09-30 15:27:22 +0900

    Add exit feature

--- echo.php
+++ echo.php
@@ -1,5 +1,7 @@
 <?php
 while (1) {
-	$str = readline('Enter any string: ');
+	$str = readline('Enter any string (If you enter \'exit\', the program will exit): ');
+
+	if ($str === 'exit') break;
 	echo $str . PHP_EOL;
 }
